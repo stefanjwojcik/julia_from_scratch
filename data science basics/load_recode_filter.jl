af5352f3833b335fba7897b68657ca7045b2f64f
Stefan Wojcik <swojcik@pewresearch.org>
2019-05-07 07:49:25 -0400

    adding new pipeline stuff

--- data science basics/load_recode_filter.jl
+++ data science basics/load_recode_filter.jl
@@ -1,12 +1,56 @@
 # The purpose of this script is to do basic data scienc
 # recodes, filters, mutates, and joins
 
-using Gadfly, DataFrames, Query, CSVFiles
+using Gadfly, DataFrames, Query, CSVFiles, DataFramesMeta, Random
 
-df = load("/users/electron/Downloads/redfin_sold_more_more.csv", quotechar='\.') |> DataFrame
+df = load("/users/electron/Downloads/redfin_sold_more_more.csv") |> DataFrame;
 
-# How to filter
-df |> @filter(_.status=="Completed")
+
+# Alter the column names - they are currently a mess
+# replace the string '.' to nothing
+alt_names = [Symbol(replace(String(x), "." => "")) for x in names(df)]
+# And actually alter the names in place
+names!(df, alt_names)
+
+# Plot lat and long
+df = df |> @mutate(location = lowercase(_.locationvalue)) |> DataFrame
+samp = rand(1:nrow(df), 400)
+plot(x=df.latLongvaluelatitude[samp],
+    y=df.latLongvaluelongitude[samp],
+    color=df.location)
+
+# quick sidebar on regex logic here::
+# Find the columns that have the underlying values
+cols_of_interest = [occursin("value", String(x)) for x in alt_names]
+# show me the columsn with actual data
+alt_names[cols_of_interest]
+# Select those that are the potentially interesting for analysis
+df |> @select(:pricevalue, :sqFtvalue, :hoavalue, :timeOnRedfinvalue)
+
+# BUT HERE IS AN EASIER WAY TO SELECT WITH REGEX!
+df_small = df |> @select(endswith("value"), 1) |> DataFrame
+
+# Keep all BUT
+df_small = df_small |> @select(-:lotSizevalue, -:streetLinevalue, -::unitNumbervalue,
+    -:photosvalue, -:pricePerSqFtvalue, -:mlsIdvalue) |> DataFrame
+
+# mutate to create a log variable and lower case of locationvalue
+df_small = df_small |>
+    @mutate(logprice = log(_.pricevalue),
+            locationvalue = lowercase(_.locationvalue)) |> DataFrame
+
+# REMOVE MISSING values
+dropmissing!(df_small)
+
+# Can als filter rows just like dplyr
+df_small |> @filter(_.hoavalue <= 500) |> DataFrame
+
+# How to group by and aggregate
+df_agg = df |>
+    @groupby(_.locationvalue) |>
+    @map({location=key(_), price=mean(_.pricevalue)}) |>
+    @orderby_descending(_.price) |>
+    DataFrame
 
 # select certain columsn
 df |> @select(:status, :endtime)
@@ -25,17 +69,6 @@ df2 = df |>
     @orderby_descending(_.b) |>
     DataFrame
 
-
-# Alter the column names
-# replace the string '.' to nothing
-# This package has some pretty cool functions
-using DataFramesMeta # for dplyr-like filtering
-using Random # for random sampling
-
-alt_names = [Symbol(replace(String(x), "." => "")) for x in names(df)]
-names!(df, alt_names)
-
-@linq df_small = df |> select(:pricevalue, :sqFtvalue)
 
 # Remove missing among the data
 comp = completecases(df_small)
