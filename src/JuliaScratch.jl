c166c868f592f2ec82b04ad3927c6b363e452b89
Stefan Wojcik <stefan.wojcik@colorado.edu>
2022-02-06 20:15:59 -0600

    creating base module

---
+++ src/JuliaScratch.jl
@@ -0,0 +1,19 @@
+module JuliaScratch
+######
+# DEPENDENCIES 
+###########
+
+##########
+## EXPORTS 
+##############
+function example()
+  print("hi")
+end
+
+export example
+
+#######
+## INCLUDE 
+###########
+
+end
